9e538f915fb005c6cdc8b28cc3597ac306dba3b8
Le Viet Nguyen Thanh <levietnguyenthanh06102004@gmail.com>
2026-06-06 15:59:47 +0700

    Sap xep project thanh BE va FE

--- src/constants/settings.js
+++
@@ -1,6 +0,0 @@
-export const CONTACT_EMAIL = "support.eyesonly@gmail.com";
-export const WEB_NAME = "EyesOnly";
-export const VERSION = "1.0.0";
-export const SOCIAL_LINKS = {
-  facebook: "https://facebook.com/eyesonly"
-};
